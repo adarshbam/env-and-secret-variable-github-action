e83a1feffe3893df1f39b8ee022294d989a47bcd
Adarsh Singh <adarshbambahadur@gmail.com>
2026-09-05 20:20:31 +0530

    Fix node error

--- Frontend/src/api/ownerSettings.api.js
+++
@@ -1,4 +0,0 @@
-import apiClient from '../lib/apiClient';
-
-export const getOwnerSettings = () => apiClient.get('/owner-settings');
-export const updateOwnerSettings = (data) => apiClient.put('/owner-settings', data);
--- Frontend/src/api/systemConfig.api.js
+++
@@ -1,4 +0,0 @@
-import apiClient from '../lib/apiClient';
-
-export const getSystemConfig = () => apiClient.get('/system-config');
-export const updateSystemConfig = (data) => apiClient.put('/system-config', data);
